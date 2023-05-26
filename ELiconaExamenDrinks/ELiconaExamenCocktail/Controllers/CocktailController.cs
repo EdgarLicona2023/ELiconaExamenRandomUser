68a94e941cb2ce1a57f19e9337fdbd7cc4dcac50
EdgarLicona2023 <ealiconagrande@gmail.com>
2023-05-26 12:23:53 -0600

    AF

---
+++ ELiconaExamenDrinks/ELiconaExamenCocktail/Controllers/CocktailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELiconaExamenCocktail.Controllers
+{
+    public class CocktailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
