657390364b3f73f8307c70c3d991288eb236b402
EdgarLicona2023 <ealiconagrande@gmail.com>
2023-05-29 17:58:57 -0600

    ADD CARDS

--- ELiconaExamenDrinks/ML/Drink.cs
+++ ELiconaExamenDrinks/ML/Drink.cs
@@ -16,6 +16,32 @@ namespace ML
         public string strInstructions { set; get; }
         public List<object> Drinks { get; set; }
         public List<Drink> drinks { get; set; }
+        public List<StrIngredient> StrIngredient { get; set; }
+        public string strIngredient1 { get; set; }
+        public string strIngredient2 { get; set; }
+        public string strIngredient3 { get; set; }
+        public string strIngredient4 { get; set; }
+        public string strIngredient5 { get; set; }
+        public string strIngredient6 { get; set; }
+        public string strIngredient7 { get; set; }
+        public string strIngredient8 { get; set; }
+        public string strIngredient9 { get; set; }
+        public string strIngredient10 { get; set; }
+
+
+
+        public List<StrMeasure> StrMeasure { get; set; }
+
+        public string strMeasure1 { set; get; }
+        public string strMeasure2 { set; get; }
+        public string strMeasure3 { set; get; }
+        public string strMeasure4 { set; get; }
+        public string strMeasure5 { set; get; }
+        public string strMeasure6 { set; get; }
+        public string strMeasure7 { set; get; }
+        public string strMeasure8 { set; get; }
+        public string strMeasure9 { set; get; }
+        public string strMeasure10 { set; get; }
 
 
         public object strDrinkAlternate { get; set; }
@@ -23,7 +49,8 @@ namespace ML
         public string strAlcoholic { get; set; }
         public string strGlass { get; set; }
         public string strTags { get; set; }
-        public string strImageSource { get; set; }
+        public string strDrinkThumb { get; set; }
+
 
     }
 }
--- ELiconaExamenDrinks/WebApplication2/Controllers/CocktelController.cs
+++ ELiconaExamenDrinks/WebApplication2/Controllers/CocktelController.cs
@@ -71,9 +71,9 @@ namespace PL.Controllers
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/");
-                //client.BaseAddress = new Uri(urlApi);
 
                 var responseTask = client.GetAsync("v1/1/search.php?s=margarita");
+
                 responseTask.Wait(); //esperar a que se resuelva la llamada al servicio
 
                 var result = responseTask.Result;
@@ -100,7 +100,28 @@ namespace PL.Controllers
                         resultItemList.strAlcoholic = resultItem.strAlcoholic;  
                         resultItemList.strGlass = resultItem.strGlass;  
                         resultItemList.strTags = resultItem.strTags;  
-                        resultItemList.strImageSource = resultItem.strImageSource;  
+                        resultItemList.strDrinkThumb = resultItem.strDrinkThumb;  
+                        resultItemList.strIngredient1 = resultItem.strIngredient1;  
+                        resultItemList.strIngredient2 = resultItem.strIngredient2;  
+                        resultItemList.strIngredient3 = resultItem.strIngredient3;  
+                        resultItemList.strIngredient4 = resultItem.strIngredient4;  
+                        resultItemList.strIngredient5 = resultItem.strIngredient5;  
+                        resultItemList.strIngredient6 = resultItem.strIngredient6;  
+                        resultItemList.strIngredient7 = resultItem.strIngredient7;  
+                        resultItemList.strIngredient8 = resultItem.strIngredient8;  
+                        resultItemList.strIngredient9 = resultItem.strIngredient9;  
+                        resultItemList.strIngredient10 = resultItem.strIngredient10;  
+
+                        resultItemList.strMeasure1 = resultItem.strMeasure1;  
+                        resultItemList.strMeasure2 = resultItem.strMeasure2;  
+                        resultItemList.strMeasure3 = resultItem.strMeasure3;  
+                        resultItemList.strMeasure4 = resultItem.strMeasure4;  
+                        resultItemList.strMeasure5 = resultItem.strMeasure5;  
+                        resultItemList.strMeasure6 = resultItem.strMeasure6;  
+                        resultItemList.strMeasure7 = resultItem.strMeasure7;  
+                        resultItemList.strMeasure8 = resultItem.strMeasure8;  
+                        resultItemList.strMeasure9 = resultItem.strMeasure9;  
+                        resultItemList.strMeasure10 = resultItem.strMeasure10;  
 
                         resultdrink.Objects.Add(resultItemList);
                     }
